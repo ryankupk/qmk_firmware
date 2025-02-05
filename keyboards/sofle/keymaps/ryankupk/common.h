f9e5d0ab12508abae4f2cf68f4eda689cf2a9768
ryankupk <ryankupk@gmail.com>
2025-02-04 22:10:37 -0700

    Update keyboard firmware code

--- keyboards/sofle/keymaps/ryankupk/common.h
+++ keyboards/sofle/keymaps/ryankupk/common.h
@@ -1,9 +1,7 @@
 #pragma once
 
 enum sofle_layers {
-    _DEFAULTS = 0,
     _QWERTY = 0,
-    _COLEMAKDH,
     _GAMING,
     _LOWER,
     _RAISE,
@@ -12,7 +10,6 @@ enum sofle_layers {
 
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
-    KC_CLMKDH,
     KC_GAMING,
     KC_LOWER,
     KC_RAISE,
