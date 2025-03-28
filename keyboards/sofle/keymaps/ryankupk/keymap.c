29e77f1996a3a390ea77006e8296886b1ef0843b
Ryan Kupka <ryankupk@gmail.com>
2025-03-28 13:58:32 -0600

    Update keymap

--- keyboards/sofle/keymaps/ryankupk/keymap.c
+++ keyboards/sofle/keymaps/ryankupk/keymap.c
@@ -29,7 +29,7 @@
 #define SHARED_LAYER LAYOUT( \
   KC_GRV,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_DEL,  KC_BSPC, \
   _______, KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     _______, _______,  KC_8,    KC_9,    KC_0,   _______, \
-  _______, KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_NO,   _______, \
+  _______, KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_NO,   KC_GRV , \
   _______, KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,_______,     _______,_______, _______, KC_NO,   KC_NO,   KC_NO,   _______, \
               _______, KC_LOWER, _______, _______, _______,    S(KC_ENT), _______, _______,  KC_HOME, KC_END \
 )
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, TD(TD_QUOTE_BACKTICK),
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOTE,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_ENT,   KC_MS_BTN1,KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_EQUAL,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_GAMING] = LAYOUT(
   //,------------------------------------------------.                    ,---------------------------------------------------.
- TD(TD_ESC),KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,                    KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
